a141af276e9f59c3befc579e2d867aed8116951c
Onur Uyar <me@onuruyar.com>
2016-03-16 11:51:01 +0000

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  cache: true,
+  context: path.join(__dirname, 'webpack', 'javascripts'),
+  entry: {
+    application: './app/index',
+    brief: './brief/index'
+  },
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: '[name].bundle.js'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.ProvidePlugin({
+      riot: 'riot'
+    })
+  ],
+  module: {
+    preLoaders: [
+      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' } }
+    ],
+    loaders: [
+      {
+        test: /\.js$|\.tag$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        // query: {
+        //   presets: ['es2015']
+        // }
+      }
+    ]
+  },
+  externals: { jquery: "jQuery" }
+}
