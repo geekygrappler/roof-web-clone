d85ff6f75fc5f8647b762d38026a1d064433f7c8
Onur Uyar <me@onuruyar.com>
2016-03-18 09:44:15 +0000

    sendgrid settings added

--- config/application.rb
+++ config/application.rb
@@ -29,3 +29,15 @@ module RoofWeb
     config.assets.initialize_on_precompile = false
   end
 end
+
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+end
