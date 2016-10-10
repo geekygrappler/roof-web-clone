dbd220aaafb2900ded3593790443d3cdbdff4590
Andy <brownie3003@gmail.com>
2016-10-10 11:24:36 +0100

    comment out cache as it's v10 of serializers and the new stuff uses v9

--- vendor/engines/roof-api/app/serializers/account_index_serializer.rb
+++ vendor/engines/roof-api/app/serializers/account_index_serializer.rb
@@ -1,5 +1,5 @@
 class AccountIndexSerializer < ActiveModel::Serializer
-  cache key: 'accounts', compress: true, expires_in: 3.hours
+  # cache key: 'accounts', compress: true, expires_in: 3.hours
   attributes :id, :email, :full_name, :user_id, :user_type, :created_at, :updated_at
 
   def full_name
