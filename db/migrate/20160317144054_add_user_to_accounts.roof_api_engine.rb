77afe5a9782ad8fcdee7445cf6e8aebecc6d20fb
Andy <brownie3003@gmail.com>
2016-10-10 12:23:05 +0100

    remove first app migrations

--- db/migrate/20160317144054_add_user_to_accounts.roof_api_engine.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from roof_api_engine (originally 20160314162519)
-class AddUserToAccounts < ActiveRecord::Migration
-  def change
-    add_reference :accounts, :user, polymorphic: true, index: true
-  end
-end
--- db/migrate/20160406115651_add_commentable_parent_to_comments.roof_api_engine.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from roof_api_engine (originally 20160406115541)
-class AddCommentableParentToComments < ActiveRecord::Migration
-  def change
-    add_column :comments, :commentable_parent_id, :integer
-    add_column :comments, :commentable_parent_type, :string
-  end
-end
