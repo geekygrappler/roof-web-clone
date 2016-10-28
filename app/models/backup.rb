60d9f473ae0ab68e0a98be2d34089ed1a844518c
Alex Dollery <alexdollery@yahoo.co.uk>
2016-10-28 21:39:24 +0100

    fix for CSV backups

--- app/models/backup.rb
+++ app/models/backup.rb
@@ -3,7 +3,7 @@ class Backup < ActiveRecord::Base
   belongs_to :backup_type
 
   def self.create_csv_backup(document)
-    backup = create(document_id: document.id, backup_type_id: BackupType.where(name: 'CSV').first.id)
+    backup = create(document_id: document.id, backup_type_id: BackupType.where(name: 'CSV').first_or_create.id)
     bucket = ROOF_AWS_RESOURCE.bucket("document-backups-#{Rails.env}")
     file_name = "csv-#{backup.id}.csv"
     save_path = Rails.root.join('tmp', file_name)
